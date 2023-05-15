6101a98203986420d556a4b7ce3d265ca22a5806
tomas <tomas.styblo@gmail.com>
2023-05-15 23:48:21 +0200

    new basic variables and zeroring and removing the dice

---
+++ script.js
@@ -0,0 +1,15 @@
+// basic variables
+var totalScore, roundScore, activePlayer, dice;
+
+totalScore = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// zeroing and removing the dice
+document.getElementById("totalScorePlayer-0").textContent = 0;
+document.getElementById("totalScorePlayer-1").textContent = 0;
+
+document.getElementById("currentScore-0").textContent = 0;
+document.getElementById("currentScore-1").textContent = 0;
+
+document.querySelector(".diceImage").style.display = "none";
