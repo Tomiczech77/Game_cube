4bae955ef9020b05679add4ed68243059c1cee3e
tomas <tomas.styblo@gmail.com>
2023-05-16 23:09:34 +0200

    random roll dice

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ document.getElementById("currentScore-0").textContent = 0;
 document.getElementById("currentScore-1").textContent = 0;
 
 document.querySelector(".diceImage").style.display = "none";
+
+document.querySelector(".rollDice").addEventListener("click", function(){
+    // random number 1-6
+    var dice = Math.ceil(Math.random()*6);
+
+    // display corect picture of dice
+    var diceElement = document.querySelector(".diceImage");
+    diceElement.style.display = "block";
+    console.log(diceElement.src = "img/" + dice + ".jpg");
+});
